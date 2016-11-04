fe3c4eed00b6ed98a3fa0e608d9edf7c247963d8
Michele Kempinsky <mkempinsky@icloud.com>
2016-11-04 14:56:17 -0700

    added challenge description in javascript

--- app/main.js
+++ app/main.js
@@ -49,6 +49,14 @@ function solution(number){
 /* ------------------------------------------------------------/
             FUNCTION FOR EXERCISE TWO
  -----------------------------------------------------------*/
+// THE CHALLENGE : 
+ /* You live in the city of Cartesia where all roads are laid out in a perfect grid. 
+ You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. 
+ The city provides its citizens with a Walk Generating App on their phones -- 
+ everytime you press the button it sends you an array of one-letter strings representing 
+ directions to walk (eg. ['n', 's', 'w', 'e']). You know it takes you one minute to traverse one city block, 
+ so create a function that will return true if the walk the app gives you will take you exactly ten minutes 
+ (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.*/
 
 function isValidWalk(walk) {
     // create an empty array to contain the walking directions
